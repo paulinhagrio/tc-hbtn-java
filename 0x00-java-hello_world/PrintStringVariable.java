f2aa2e456cc6d5df550de2c760cea6dd6ad7e56b
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-02 16:18:26 -0300

    utilizando substring

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String texto = "Holberton School";
+        System.out.printf("%s\n%s\n%s\n", texto, texto, texto.substring(0,9));
+    }
+}
