643827d0b4f4c6291955afa280406068f21db266
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-02 15:48:28 -0300

    apagando linhas desnecessarias

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,10 +1,6 @@
 public class PrintFloatVariable {
-
     public static void main(String[] args) {
-
-
         float pi = 3.1419f;
-
         System.out.printf("Valor de pi: %.3f\n", pi);
     }
 }
