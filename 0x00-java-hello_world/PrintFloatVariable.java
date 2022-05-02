698c3c331d137860aa511e5da426163a7543f99e
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-02 15:43:08 -0300

    utilizando o float

---
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -0,0 +1,11 @@
+public class PrintFloatVariable {
+
+    public static void main(String[] args) {
+
+
+        float pi = 3.1419f;
+
+        System.out.printf("Valor de pi: %.3f\n", pi);
+    }
+}
+
