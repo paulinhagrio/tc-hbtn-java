d17807b25f0077589df8ca34f55d714cdd8a1ca0
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-03 15:16:53 -0300

    Numeros de 0a99

---
+++ 0x01-numeros/Numeros0a99.java
@@ -0,0 +1,9 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        int num = 99;
+        for (int i = 0; i < 99; i++) {
+            System.out.printf(i + ", ");
+        }
+        System.out.printf("%d\n", num);
+    }
+}
