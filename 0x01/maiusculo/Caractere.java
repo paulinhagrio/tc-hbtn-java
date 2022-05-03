ceeae5a2bd35afd9d23ea24c515fc9f03af1719a
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-03 17:11:50 -0300

    Letras maiúsculas

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,10 @@
+public class Caractere {
+    public static boolean EhMaiusculo(char c) {
+        if (65 <= c && c <= 90)
+            return true;
+         else
+            return false;
+
+    }
+}
+
