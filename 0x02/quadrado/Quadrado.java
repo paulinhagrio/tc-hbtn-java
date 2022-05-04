fe28f0cd3e6b33f6e1f36bc26d24fdccedaa3389
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-04 15:29:55 -0300

    Area Quadrado

---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,12 @@
+package quadrado;
+
+public class Quadrado {
+    public static double area(double lado) throws IllegalArgumentException{
+
+        if (lado <= 0) {
+            throw new IllegalArgumentException("Lado deve possuir valor positivo");
+        }
+
+        return lado * lado;
+    }
+}
