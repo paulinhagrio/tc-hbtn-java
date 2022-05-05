653f2238179c4dfea027efdae104bc11030f1b24
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-05 12:04:27 -0300

    Livraria

---
+++ 0x03/livraria/Livro.java
@@ -0,0 +1,45 @@
+
+import exceptions.*;
+
+
+public class Livro {
+    String titulo;
+    String autor;
+    double preco;
+
+
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public String getTitulo() throws LivroInvalidoException {
+        if (titulo.length() < 3){
+            throw new LivroInvalidoException("Titulo de livro invalido");
+        }
+        return titulo;
+    }
+
+
+
+    public String getAutor() throws AutorInvalidoException {
+
+        if (!(autor.contains(" ")) ){
+            throw new AutorInvalidoException("Nome de autor invalido");
+        }
+        return autor;
+    }
+
+
+
+    public double getPreco() throws LivroInvalidoException {
+
+        if (preco <= 0){
+            throw new LivroInvalidoException("Preco de livro invalido");
+            }
+        return preco;
+    }
+}
+
---
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -0,0 +1,12 @@
+import exceptions.*;
+
+public class LivroEdicaoOuro extends Livro {
+    public LivroEdicaoOuro(String titulo, String autor, double preco) {
+        super(titulo, autor, preco);
+    }
+
+    public double getPreco() {
+
+        return   preco + (preco * 0.3);
+    }
+}
