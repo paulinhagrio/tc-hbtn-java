e44e2cf8e38e98c73dd2e06ebc8ec10b35225b29
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-06 17:14:40 -0300

    Status do Personagem

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,51 @@
+
+
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+
+        if (getSaudeAtual() > 0){
+           this.status = "vivo";
+        }else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public  int tomarDano(int quantidadeDeDano){
+       if (saudeAtual - quantidadeDeDano < 0){
+           setSaudeAtual(0);
+       } else {
+           setSaudeAtual(getSaudeAtual() - quantidadeDeDano);
+       }
+        return  saudeAtual;
+    }
+
+    public int receberCura(int quantidadeDeCura){
+            if(saudeAtual + quantidadeDeCura > 100) {
+                setSaudeAtual(100);
+            }else{
+                setSaudeAtual(getSaudeAtual() + quantidadeDeCura);
+            }
+        return saudeAtual;
+    }
+}
---
+++ 0x04/status/Program.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+        PersonagemGame personagem1 = new PersonagemGame();
+
+        personagem1.setNome("Sonic");
+        personagem1.setSaudeAtual(0);
+
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+
+        personagem1.receberCura(90);
+
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+
+        personagem1.tomarDano(25);
+
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+
+        personagem1.tomarDano(80);
+
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+
+        personagem1.receberCura(10);
+
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+    }
+}
