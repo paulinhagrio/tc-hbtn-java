25d32e41451c2129b5115e022807e205e6df02e5
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-05 21:32:13 -0300

    Conta Bancaria Controlada e Tarifada

---
+++ 0x03/contas_especiais/ContaBancariaControlada.java
@@ -0,0 +1,25 @@
+
+public class ContaBancariaControlada extends ContaBancariaBasica{
+
+    double saldoMinimo;
+    double valorPenalidade;
+
+    public ContaBancariaControlada(String numeracao, double taxaJurosAnual, double saldoMinimo, double valorPenalidade) {
+        super(numeracao, taxaJurosAnual);
+        this.numeracao = numeracao;
+        this.taxaJurosAnual = taxaJurosAnual;
+        this.saldoMinimo = saldoMinimo;
+        this.valorPenalidade = valorPenalidade;
+
+    }
+
+    @Override
+    public void aplicarAtualizacaoMensal(){
+        if (saldo < saldoMinimo){
+            if (saldo == 0){
+                this.saldo -= valorPenalidade;
+            }
+
+        }
+    }
+}
---
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -0,0 +1,42 @@
+import exceptions.*;
+
+public class ContaBancariaTarifada extends ContaBancariaBasica {
+    int quantidadeTransacoes;
+    double valorPenalidade;
+
+    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
+        super(numeracao, taxaJurosAnual);
+    }
+
+
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        if (valor <= 0) {
+            throw new OperacaoInvalidaException("Valor de saque deve ser menor que 0");
+        }
+        if (getSaldo() < valor){
+            throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+        }
+
+        if (getQuantidadeTransacoes() <= 0){
+            valor += valorPenalidade;
+        }
+
+        saldo -= valor;
+    }
+
+    public void depositar(double valor)throws OperacaoInvalidaException {
+        if (valor <= 0){
+            throw new OperacaoInvalidaException ("Valor para deposito deve ser maior que 0");
+        }
+
+        if (getQuantidadeTransacoes() <= 0){
+            valor += valorPenalidade;
+        }
+        saldo += valor;
+
+    }
+
+    public int getQuantidadeTransacoes() {
+        return quantidadeTransacoes;
+    }
+}
