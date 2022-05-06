31eedbd4f644fe04142da15aa1db68978e94fac5
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-06 15:35:32 -0300

    Métodos

---
+++ 0x04/metodos/PersonagemGame.java
@@ -0,0 +1,42 @@
+
+
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public  int tomarDano(int quantidadeDeDano){
+
+       if (saudeAtual - quantidadeDeDano < 0){
+           saudeAtual = 0;
+       }else {
+           saudeAtual -= quantidadeDeDano;
+       }
+        return  saudeAtual;
+    }
+
+    public int receberCura(int quantidadeDeCura){
+
+            if(saudeAtual + quantidadeDeCura > 100){
+                saudeAtual = 100;
+            }else{
+                saudeAtual += quantidadeDeCura;
+            }
+        return saudeAtual;
+    }
+}
---
+++ 0x04/metodos/Program.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+        PersonagemGame personagem1 = new PersonagemGame();
+
+        personagem1.setNome("Sonic");
+        personagem1.setSaudeAtual(10);
+
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
+
+        personagem1.receberCura(91);
+
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
+
+        personagem1.tomarDano(25);
+
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
+
+        personagem1.tomarDano(80);
+
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
+    }
+}
