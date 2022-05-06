4815b2d67b45bacf841540dc11658be0e42c5024
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-06 14:06:53 -0300

    Encapsulamento

---
+++ 0x04/encapsulamento/Program.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+        PersonagemGame personagem1 = new PersonagemGame();
+
+        personagem1.setNome("Sonic");
+        personagem1.setSaudeAtual(10);
+
+        System.out.printf("Nome: %s - Saude: %d\n", personagem1.getNome(), personagem1.getSaudeAtual());
+    }
+}
