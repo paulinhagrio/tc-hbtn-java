9f1d476a92e331d07a93312f53dca83e01d1fd10
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-09 17:04:21 -0300

    Retângulo_2

--- 0x05/retangulo/Program.java
+++ 0x05/retangulo/Program.java
@@ -1,8 +1,8 @@
-import java.util.Locale;
+
 
 public class Program {
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("pt", "BR"));
+
         Retangulo retangulo = new Retangulo();
         retangulo.setLargura(10);
         retangulo.setAltura(2);
