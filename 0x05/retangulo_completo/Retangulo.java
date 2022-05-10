b14b9bdc8107ed61fb73c5725def019f1ad395aa
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-10 09:13:53 -0300

    Retângulo Completo

---
+++ 0x05/retangulo_completo/Retangulo.java
@@ -0,0 +1,43 @@
+
+
+public class Retangulo extends FormaGeometrica{
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) throws IllegalArgumentException{
+        if (getLargura() < 0){
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+
+        }
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (getAltura() < 0){
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+
+    public double area(){
+        double area;
+        area = getAltura() * getLargura();
+        return area;
+    }
+
+
+    @Override
+    public String toString(){
+        return  String.format( "[Retangulo] %.2f / %.2f", largura, altura);
+        // System.out.println("[Retangulo]" + largura + altura );
+    }
+}
+
