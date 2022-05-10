f959e5cf3a097d83bd223af4cab18c19057a1eeb
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-10 10:42:30 -0300

    Quadrado

---
+++ 0x05/quadrado/Quadrado.java
@@ -0,0 +1,23 @@
+package quadrado;
+
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) throws IllegalArgumentException {
+        if (getLado() < 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        } else {
+            this.lado = lado;
+            this.altura = lado;
+            this.largura = lado;
+        }
+    }
+
+    public String toString() {
+        return String.format("[Quadrado] %.2f", lado);
+    }
+}
