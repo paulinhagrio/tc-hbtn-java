eb19473e8ce1586cd3f75f89df110ceab0e30d70
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-10 18:07:07 -0300

    Pedido Livraria

---
+++ 0x06/pedido_livraria/Pedido.java
@@ -0,0 +1,31 @@
+import produtos.*;
+
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void setPercentualDesconto(double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public double calcularTotal(){
+        double totalProdutos = 0;
+        for (int i=0; i< itens.length; i++){
+            totalProdutos += itens[i].getProduto().obterPrecoLiquido() * itens[i].getQuantidade();
+        }
+
+        double desconto = totalProdutos * (getPercentualDesconto()/100);
+        totalProdutos -= desconto;
+
+        return totalProdutos;
+    }
+}
