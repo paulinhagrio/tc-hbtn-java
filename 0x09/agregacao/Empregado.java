dde199ea54f1800b90abcaff242afdc698816a27
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-16 16:06:52 -0300

    Empregado/Endereço

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,45 @@
+
+
+public class Empregado {
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void apresentar(){
+        System.out.println("Codigo:" + getCodigo());
+        System.out.println("Nome:" + getNome());
+        System.out.println("Bairro:" + getEndereco().getBairro());
+        System.out.println("Cidade:" + getEndereco().getCidade());
+        System.out.println("Pais:" + getEndereco().getPais());
+    }
+}
