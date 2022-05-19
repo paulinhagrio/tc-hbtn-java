740988ba24e51e3966693651515281713aff916e
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-19 11:22:39 -0300

    Array

---
+++ List/numeros/ManipularArrayNumeros.java
@@ -0,0 +1,41 @@
+
+
+import java.util.List;
+
+public class ManipularArrayNumeros {
+
+    public static int buscarPosicaoNumero(List<Integer> numeros, int i) {
+
+        for (int j = 0; j < numeros.size(); j++) {
+            if (numeros.get(j) == i) {
+                return j;
+            }
+        }
+                return -1;
+    }
+
+    public static void adicionarNumero(List<Integer> numeros, int i){
+
+        if (buscarPosicaoNumero(numeros, i) != -1){
+            throw new IllegalArgumentException("Numero jah contido na lista");
+        }else {
+            numeros.add(i);
+        }
+    }
+
+    public static void removerNumero(List<Integer> numeros, int i){
+        if (buscarPosicaoNumero(numeros, i) == -1){
+            throw new IllegalArgumentException("Numero nao encontrado na lista");
+        }else {
+            numeros.remove(buscarPosicaoNumero(numeros,i));
+        }
+    }
+
+    public static void substituirNumero(List<Integer> numeros, int numeroSubstituir, int numeroSubstituto){
+        if (buscarPosicaoNumero(numeros,numeroSubstituir) == -1){
+            numeros.add(numeroSubstituto);
+        }else{
+            numeros.set(buscarPosicaoNumero(numeros, numeroSubstituir), numeroSubstituto);
+        }
+    }
+}
