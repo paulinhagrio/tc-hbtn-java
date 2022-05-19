71178d4a6472268a969f1cbca4eb1888c285e462
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-19 13:41:05 -0300

    Cookies

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,36 @@
+
+
+import java.util.ArrayList;
+
+public class Pedido {
+
+    ArrayList<PedidoCookie> cookies;
+
+    public Pedido() {
+        this.cookies = new ArrayList<PedidoCookie>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie){
+        cookies.add(pedidoCookie);
+    }
+
+
+    public int obterTotalCaixas(){
+        int quantidadeTotal = 0;
+        for (PedidoCookie pedido: cookies ) {
+            quantidadeTotal += pedido.getQuantidadeCaixas();
+        }
+        return quantidadeTotal;
+    }
+
+    public int removerSabor(String sabor){
+        int quantidadeTotalCaixasRemovidas = 0;
+        for (int i=0; i<cookies.size(); i++) {
+            if (cookies.get(i).getSabor().equals(sabor)){
+                quantidadeTotalCaixasRemovidas += cookies.get(i).getQuantidadeCaixas();
+                cookies.remove(i);
+            }
+        }
+        return quantidadeTotalCaixasRemovidas;
+    }
+}
