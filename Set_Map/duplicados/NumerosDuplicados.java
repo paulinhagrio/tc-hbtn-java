b1535505d4b6d3378664b8ed65de5b2fd1b39a82
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-25 17:31:47 -0300

    Duplicados

---
+++ Set_Map/duplicados/NumerosDuplicados.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class NumerosDuplicados {
+
+    public static TreeSet<Integer> buscar(int[] numeros){
+
+        List<Integer> list = Arrays.stream(numeros).boxed().collect(Collectors.toList());
+
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        List<Integer> filtrarDuplicados = list.stream()
+                .filter(i -> !hashSet.add(i))
+                .collect(Collectors.toList());
+
+        return new TreeSet<>(filtrarDuplicados);
+    }
+}
