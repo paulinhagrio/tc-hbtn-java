85ea5dd76b414f0e02ad120309271d908dc3bc57
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-25 16:59:24 -0300

    Contagem de Palavras

---
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -0,0 +1,30 @@
+
+
+
+import java.util.TreeMap;
+
+
+public class AnalisadorFrase {
+
+    public static TreeMap<String, Integer> contagemPalavras(String palavras) {
+        TreeMap<String, Integer> map = new TreeMap<>();
+        palavras = palavras.replace("?", "");
+        palavras = palavras.replace(".", "");
+        palavras = palavras.replace("!", "");
+
+        String[] teste = palavras.split(" ");
+
+        int cont = 0;
+
+        for (int i = 0; i < teste.length; i++) {
+            for (int j = 0; j < teste.length; j++) {
+                if (teste[i].equalsIgnoreCase(teste[j])) {
+                    cont++;
+                }
+            }
+            map.put(teste[i].toLowerCase(), cont);
+            cont = 0;
+        }
+        return map;
+    }
+}
