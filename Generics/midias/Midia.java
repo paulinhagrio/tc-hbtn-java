3728ce14186e3afa5396cb3f8c099e2c4ca49d27
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-05-30 17:27:57 -0300

    Midias

---
+++ Generics/midias/Midia.java
@@ -0,0 +1,22 @@
+
+
+public class Midia {
+    private String nome;
+
+        public Midia() {
+    }
+
+    public Midia(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Tipo: %s - Nome: %s", this.getClass().getName(), getNome());
+    }
+}
