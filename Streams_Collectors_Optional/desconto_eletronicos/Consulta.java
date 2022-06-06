ebe46912ccdf5e60fb4dc67a9f8101e1800600ab
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-06-06 13:15:42 -0300

    Eletronicos com 15% de desconto

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,35 @@
+
+
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+        return pedido.getProdutos().stream().filter(t -> t.getCategoria().equals(CategoriaProduto.LIVRO)).collect(
+                Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+        return produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed()).findFirst().orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMin){
+        return produtos.stream().filter(p -> p.getPreco() >= precoMin).collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos){
+        return pedidos.stream().filter(p -> p.getProdutos().stream().anyMatch(t -> t.getCategoria().equals(
+                CategoriaProduto.ELETRONICO))).collect(Collectors.toList());
+    }
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        return produtos.stream().map(produto -> { if (produto.getCategoria().equals(CategoriaProduto.ELETRONICO)) {
+                produto.setPreco(produto.getPreco() - produto.getPreco() * 0.15);
+            }
+            return produto;
+        }).collect(Collectors.toList());
+    }
+}
