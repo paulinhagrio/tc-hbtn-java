5f6fa07c4f146f29a68f34a02c6ad6b348d4fdca
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-06-06 17:57:59 -0300

    Hobbies por cargo

---
+++ Streams_Collector_Optional_exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(
+            List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,() -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas){
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> todasPessoas){
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> todasPessoas){
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.averagingDouble(Pessoa::getSalario)));
+    }
+
+    public static Map<String,TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> todasPessoas){
+       return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+               Collectors.flatMapping(p -> p.getHobbies().stream(), Collectors.toCollection(TreeSet::new))));
+
+    }
+}
