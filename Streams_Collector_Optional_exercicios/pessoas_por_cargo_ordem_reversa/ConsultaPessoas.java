7e56bc2f95b90645585b35b56c5c19e028125bd0
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-06-06 16:41:02 -0300

    Pessoas agrupadas por cargos em ordem reversa

--- Streams_Collector_Optional_exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
+++ Streams_Collector_Optional_exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -6,9 +6,6 @@ import java.util.stream.Collectors;
 public class ConsultaPessoas {
     public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(
             List<Pessoa> todasPessoas) {
-        return todasPessoas.stream()
-                .collect(Collectors
-                        .groupingBy(Pessoa::getCargo,() -> new TreeMap<>(Comparator.reverseOrder()), Collectors
-                                .toCollection(TreeSet::new)));
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,() -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
     }
 }
